5739e6d154e42e5044b835f147a11f32e7a1c6ed
Shenghao Chen <shenghaoc@outlook.com>
2023-01-15 02:38:26 +0800

    Add basic selection

--- app.vue
+++ app.vue
@@ -1,3 +1,15 @@
+<script setup>
+import { ID_INJECTION_KEY } from 'element-plus'
+provide(ID_INJECTION_KEY, {
+  prefix: 100,
+  current: 0,
+})
+</script>
+
 <template>
   <h1>Price Prediction</h1>
+<AppForm>
+  This is an auto-imported component.
+</AppForm>
 </template>
+
---
+++ components/AppForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-form :model="form" label-width="120px">
+    <el-form-item label="ML Model">
+      <el-select-v2 v-model="form.ml_model" 
+      :options="options"
+      placeholder="Please select"
+      size="large"
+      />
+    </el-form-item>
+
+  </el-form>
+</template>
+
+<script lang="ts" setup>
+import { reactive } from 'vue'
+
+import {
+  ElSelectV2,
+  ElForm,
+  ElFormItem,
+} from "element-plus";
+
+// do not use same name with ref
+const form = reactive({
+  ml_model: ''
+})
+
+const options = [
+  {
+    value: 'Support Vector Regression',
+    label: 'Support Vector Regression',
+  },
+  {
+    value: 'Ridge Regression',
+    label: 'Ridge Regression',
+  },
+]
+</script>
